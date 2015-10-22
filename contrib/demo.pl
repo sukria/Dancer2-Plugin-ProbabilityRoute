f0d8f5ec069b998ec9777e2b48938391a9ba1364
Alexis Sukrieh <sukria@sukria.net>
2015-10-22 15:17:17 +0200

    the demo app with the new API

--- contrib/demo.pl
+++ contrib/demo.pl
@@ -3,17 +3,12 @@
 use Dancer2;
 use Dancer2::Plugin::ProbabilityRoute;
 
-probability_route 33, 'get', '/' => sub {
-    "One chance on 3 ! Well done"
-};
-
-probability_route 67, 'get', '/' => sub {
-    "Like two thirds of the world... Boring.";
-};
+get '/' => probability
+    33 => sub { "1/3 good job!"},
+    67 => sub { "2/3, so common..." };
 
 get '/showme' => sub {
     "Your score is : ".probability_user_score;
 };
 
-declare_probability_routes;
 start;
